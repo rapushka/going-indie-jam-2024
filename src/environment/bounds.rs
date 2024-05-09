bb4175e3de5434e3b4bbee9c5a0c0778c4894079
Rapuska <mikalajsiatkouski@gmail.com>
2024-05-09 16:40:23 +0300

    second bound spawned

--- src/environment/bounds.rs
+++ src/environment/bounds.rs
@@ -2,9 +2,11 @@ use bevy::ecs::system::Command;
 use bevy::prelude::*;
 
 use crate::AppState;
-use crate::environment::bounds::command::SpawnChunk;
+use crate::environment::bounds::command::SpawnChunkCommand;
 
 mod command;
+
+const CHUNK_TRANSPARENCY: u8 = 100;
 
 #[derive(Component)]
 pub struct Chunk(u8);
@@ -24,9 +26,17 @@ impl Plugin for BoundsPlugin {
 fn create_bounds(
     mut commands: Commands,
 ) {
-    let color = Color::rgba_u8(255, 0, 0, 100);
-    let position = Vec3::new(0.0, 0.0, -5.0);
-    let size = Vec3::new(30.0, 20.0, 10.0);
+    commands.add(SpawnChunkCommand {
+        chunk_index: 0,
+        color: Color::rgba_u8(255, 0, 0, CHUNK_TRANSPARENCY),
+        position: Vec3::new(0.0, 0.0, -5.0),
+        size: Vec3::new(20.0, 20.0, 10.0),
+    });
 
-    commands.add(SpawnChunk { chunk_index: 0, color, position, size })
+    commands.add(SpawnChunkCommand {
+        chunk_index: 1,
+        color: Color::rgba_u8(0, 255, 0, CHUNK_TRANSPARENCY),
+        position: Vec3::new(0.0, 0.0, 10.0),
+        size: Vec3::new(20.0, 20.0, 20.0),
+    });
 }
--- src/environment/bounds/command.rs
+++ src/environment/bounds/command.rs
@@ -3,14 +3,14 @@ use bevy::prelude::*;
 use bevy::render::primitives::Aabb;
 use crate::environment::bounds::Chunk;
 
-pub struct SpawnChunk {
+pub struct SpawnChunkCommand {
     pub chunk_index: u8,
     pub color: Color,
     pub position: Vec3,
     pub size: Vec3,
 }
 
-impl Command for SpawnChunk {
+impl Command for SpawnChunkCommand {
     fn apply(self, world: &mut World) {
         let mesh = world.resource_mut::<Assets<Mesh>>().add(Cuboid::from_size(self.size));
         let material = world.resource_mut::<Assets<StandardMaterial>>().add(self.color);
