6d62c0747135dc8f828954dc9786ac32b6e18896
Rapuska <mikalajsiatkouski@gmail.com>
2024-05-12 20:25:59 +0300

    two platforms

--- src/ui/level_selection/levels/level_1.rs
+++ src/ui/level_selection/levels/level_1.rs
@@ -11,5 +11,7 @@ pub fn load(
     commons::create_spawn_point(parent, 0, Vec3::new(0.0, 0.1, 0.0));
     commons::create_ground(parent, assets, Vec3::ZERO, Vec3::new(3.0, 1.0, 3.0));
 
-    commons::create_ground(parent, assets, Vec3::new(4.0, 3.0, 3.0), Vec3::new(3.0, 1.0, 3.0));
+    commons::create_ground(parent, assets, Vec3::new(7.0, 2.0, 1.0), Vec3::new(3.0, 1.0, 3.0));
+
+    commons::create_ground(parent, assets, Vec3::new(3.0, 6.5, 9.0), Vec3::new(3.0, 1.0, 3.0));
 }
