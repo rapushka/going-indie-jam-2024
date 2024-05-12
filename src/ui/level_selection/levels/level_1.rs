1fd1532e1f1d675583951b3335b8196f74eaf210
Rapuska <mikalajsiatkouski@gmail.com>
2024-05-12 20:32:39 +0300

    level d e s i g n

--- src/ui/level_selection/levels/level_1.rs
+++ src/ui/level_selection/levels/level_1.rs
@@ -14,4 +14,6 @@ pub fn load(
     commons::create_ground(parent, assets, Vec3::new(7.0, 2.0, 1.0), Vec3::new(3.0, 1.0, 3.0));
 
     commons::create_ground(parent, assets, Vec3::new(3.0, 6.5, 9.0), Vec3::new(3.0, 1.0, 3.0));
+
+    commons::create_ground(parent, assets, Vec3::new(-11.0, 8., 9.0), Vec3::new(6.0, 1.0, 3.0));
 }
