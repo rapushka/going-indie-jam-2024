73519e902e2ec10a1f8d655c3ea8249997faa4c2
Rapuska <mikalajsiatkouski@gmail.com>
2024-05-14 00:20:01 +0300

    new stars placement

--- src/ui/level_selection/levels/level_1.rs
+++ src/ui/level_selection/levels/level_1.rs
@@ -15,18 +15,20 @@ pub fn load(
     create_spawn_point(root, 0, Vec3::new(0.0, 0.1, 0.0));
     create_ground(root, assets, Vec3::ZERO, Vec3::new(3.0, 1.0, 3.0));
 
+    create_star(root, assets, Vec3::new(7.0, 4.5, 1.0));
     create_ground(root, assets, Vec3::new(7.0, 2.0, 1.0), Vec3::new(3.0, 1.0, 3.0));
 
+    create_star(root, assets, Vec3::new(3.0, 9.0, 9.0));
     create_ground(root, assets, Vec3::new(3.0, 6.5, 9.0), Vec3::new(3.0, 1.0, 3.0));
 
+    create_invisible_wall(root, Vec3::new(-3.0, 16.5, 9.0), Vec3::new(0.25, 9.0, 10.0));
     create_spawn_point(root, 1, Vec3::new(-9.0, 8.1, 9.0));
     create_ground(root, assets, Vec3::new(-11.0, 8.0, 9.0), Vec3::new(4.0, 1.0, 3.0));
+
+    create_star(root, assets, Vec3::new(-20.0, 10.5, 2.0));
     create_ground(root, assets, Vec3::new(-20.0, 8.0, 6.0), Vec3::new(3.0, 1.0, 6.0));
-    create_invisible_wall(root, Vec3::new(-3.0, 16.5, 9.0), Vec3::new(0.25, 9.0, 10.0));
 
     create_chunk(root, meshes, materials, 1, Color::RED, vec2(-20.0, 6.0), vec2(12.0, 24.0));
-
-    create_star(root, assets, Vec3::new(3.0, 3.0, 3.0));
 }
 
 fn vec3(x: f32, y: f32, z: f32) -> Vec3 { Vec3::new(x, y, z) }
