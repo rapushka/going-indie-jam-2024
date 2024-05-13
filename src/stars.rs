ef5a39945dfa2b682d66b5cc16e64bcc4d17015e
Rapuska <mikalajsiatkouski@gmail.com>
2024-05-13 21:29:36 +0300

    stars collecting

--- src/stars.rs
+++ src/stars.rs
@@ -1,5 +1,10 @@
 use bevy::prelude::*;
+use bevy_rapier3d::plugin::RapierContext;
+
 pub use create::*;
+
+use crate::{AppState, Order};
+use crate::player::Player;
 
 mod create;
 
@@ -11,7 +16,27 @@ pub struct StarsPlugin;
 impl Plugin for StarsPlugin {
     fn build(&self, app: &mut App) {
         app
+            .add_systems(PreUpdate, (
+                collect_stars,
+            )
+                .in_set(Order::GameLogic)
+                .run_if(in_state(AppState::Gameplay)))
         ;
+    }
+}
+
+fn collect_stars(
+    mut commands: Commands,
+    rapier_context: Res<RapierContext>,
+    players: Query<Entity, With<Player>>,
+    stars: Query<Entity, With<Star>>,
+) {
+    for player in players.iter() {
+        for star in stars.iter() {
+            if rapier_context.contact_pair(player, star).is_some() {
+                commands.entity(star).despawn_recursive();
+            }
+        }
     }
 }
 
